2b51de4814dc3314d3579ffe43d1d21e805ed267
aeee <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2021-12-30 16:03:18 +0700

    RP NoArrest submodules, freeze submodules

--- lua/physgun_impr/server/modules/god.lua
+++ lua/physgun_impr/server/modules/god.lua
@@ -1,6 +1,10 @@
 local mod = PhysImpr.Modules.Register("God", "God")
 mod.Name = "Held players are immune to damage"
 
+local arrest = mod:AddSubModule("NoArrest")
+arrest.Name = "[experimental] Held players cannot be arrested"
+arrest.IsRP = true
+
 local sfx_ric = {
 	"weapons/fx/rics/ric1.wav",
 	"weapons/fx/rics/ric2.wav",
@@ -15,5 +19,13 @@ hook.Add("PlayerShouldTakeDamage", "PhysImpr_NoDying", function(ply, atk)
 		local snd = sfx_ric[math.random(#sfx_ric)]
 		ply:EmitSound(snd, 70, math.random(90, 110), 0.8)
 		return false
+	end
+end)
+
+hook.Add("canArrest", "PhysImpr_HoldNoArrest", function(arrester, arrestee)
+	if not mod:GetSubState(arrest) then return end
+
+	if PhysImpr.IsHeld(arrestee) then
+		return false, "Arresting physgunned players was disabled."
 	end
 end)
--- lua/physgun_impr/server/modules/quickfreeze.lua
+++ lua/physgun_impr/server/modules/quickfreeze.lua
@@ -1,6 +1,16 @@
 local mod = PhysImpr.Modules.Register("PlayerFreeze", "PlayerFreeze")
 mod.Name = "Freeze players via RMB"
 
+local nomove = mod:AddSubModule("DisableMovement")
+nomove.Name = "Freeze players in-place, disabling movement entirely"
+
+local nodmg = mod:AddSubModule("Damage")
+nodmg.Name = "Frozen players don't take damage"
+
+local noarr = mod:AddSubModule("NoArrest")
+noarr.Name = "[experimental] Frozen players cannot be arrested"
+noarr.IsRP = true
+
 PhysImpr.Frozen = {}
 
 function PhysImpr.FreezeEffect(ply)
@@ -35,13 +45,21 @@ hook.Add("PhysImpr_ReleasePlayer", "PhysImpr_FreezeUntrack", function(adm, ent)
 	PhysImpr.UnfreezeIfFrozen(adm)
 end)
 
+hook.Add("FinishMove", "PhysImpr_Freeze", function(ply, mv)
+	if not mod.State then return end
+	if not nomove.State then return end
+	if not PhysImpr.Frozen[ply] then return end
+
+	return true -- sauce engine drops the player slowly unless i do this
+end)
+
 -- Using SetupMove because addons may decide to prevent keys from StartCommand
 hook.Add("SetupMove", "PhysImpr_Freeze", function(ply, mv, cmd)
 	if not mod.State then return end
 
+	-- dumb bug where frozen players get negative vertical velocity,
+	-- even if held by physgun             VVV
 	if PhysImpr.Frozen[ply] and PhysImpr.IsHeld(ply) then
-		-- dumb bug where frozen players get negative vertical velocity,
-		-- even if held by physgun
 		mv:SetVelocity(vector_origin)
 		return
 	end
@@ -52,8 +70,34 @@ hook.Add("SetupMove", "PhysImpr_Freeze", function(ply, mv, cmd)
 	if bit.band(cmd:GetButtons(), IN_ATTACK2) == 0 then return end
 
 	-- RMB pressed, do the epic freeze
-	PhysImpr.TryFreeze(ply, tgt, false)
-	PhysImpr.FreezeEffect(tgt)
-	PhysImpr.Frozen[tgt] = engine.TickCount()
+	local ok = PhysImpr.TryFreeze(ply, tgt, false)
+	if ok then
+		PhysImpr.FreezeEffect(tgt)
+		PhysImpr.Frozen[tgt] = engine.TickCount()
+	end
 end)
 
+local sfx_ric = {
+	"physics/glass/glass_impact_bullet1.wav",
+	"physics/glass/glass_impact_bullet2.wav",
+	"physics/glass/glass_impact_bullet3.wav",
+	"physics/glass/glass_impact_bullet4.wav",
+}
+
+hook.Add("PlayerShouldTakeDamage", "PhysImpr_FreezeDamage", function(ply, atk)
+	if not mod:GetSubState(nodmg) then return end
+
+	if PhysImpr.Frozen[ply] then
+		local snd = sfx_ric[math.random(#sfx_ric)]
+		ply:EmitSound(snd, 70, math.random(90, 110), 0.8)
+		return false
+	end
+end)
+
+hook.Add("canArrest", "PhysImpr_FreezeNoArrest", function(arrester, arrestee)
+	if not mod:GetSubState(noarr) then return end
+
+	if PhysImpr.Frozen[arrestee] then
+		return false, "Arresting frozen players was disabled."
+	end
+end)
